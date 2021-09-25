ec998eb7e2924c8c345d238ded49ced3ed8d50a9
Tanner <tanner2379@live.com>
2021-09-24 17:57:54 -0700

    updated cors production mode

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "MY_DOMAIN.com"
+    origins "https://highsoaringministries.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
