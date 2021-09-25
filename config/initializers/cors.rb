a8c2ebffab296644fbb5184da2a11626f2da9027
Tanner <tanner2379@live.com>
2021-09-24 21:10:21 -0700

    fixed syntax error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://highsoaringministries.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
   allow do
     origins "http://highsoaringministries.herokuapp.com"
