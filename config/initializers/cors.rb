55ff2e3473625877de39120d7fcfd7b9b7f4bf5a
Tanner Sigel <tanner2379@live.com>
2021-10-23 18:04:22 -0700

    Added www to cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,20 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins "https://www.highsoaringministries.herokuapp.com"
+    resource "*",
+    headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
+
+  allow do
+    origins "http://www.highsoaringministries.herokuapp.com"
+    resource "*",
+    headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
+
+  allow do
     origins "https://highsoaringministries.herokuapp.com"
     resource "*",
     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
