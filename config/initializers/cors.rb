eb298917593b2f2d92ba2a7ac8614bc30c1e97d9
Tanner Sigel <tanner2379@live.com>
2021-10-30 12:11:52 -0700

    removed cors wildcard

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -41,11 +41,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
-
-  allow do
-    origins "*"
-    resource "*",
-    headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    credentials: true
-  end
 end
