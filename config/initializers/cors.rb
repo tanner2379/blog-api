fcf805e8299cb291a5af286491573410ced7786f
Tanner Sigel <tanner2379@live.com>
2021-10-30 11:43:45 -0700

    added heroku dns from front end to cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -34,4 +34,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
+
+  allow do
+    origins "secret-melon-nagt1bephe8j9m5gs1g4h27u.herokudns.com"
+    resource "*",
+    headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
 end
