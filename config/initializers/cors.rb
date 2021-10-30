b03df37dcb30e49f85704f0216753bee0ac10659
Tanner Sigel <tanner2379@live.com>
2021-10-30 11:50:43 -0700

    still attempting to fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -29,7 +29,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "http://highsoaringministries.herokuapp.com"
+    origins "http://www.highsoaringministries.herokuapp.com"
     resource "*",
     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
