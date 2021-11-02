2d793d9817e87637d74c5ed9838e1f5b1d70b0ff
Tanner Sigel <tanner@barnpros.com>
2021-11-02 14:30:22 -0700

    refactored cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,46 +1,14 @@
-# require 'rack/cors'
+require 'rack/cors'
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "http://localhost:3000"
-#     resource "*",
-#     headers: :any,
-#     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#     credentials: true
-#   end
-
-#   allow do
-#     origins "https://www.highsoaringministries.herokuapp.com"
-#     resource "*",
-#     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#     credentials: true
-#   end
-
-#   allow do
-#     origins "http://www.highsoaringministries.herokuapp.com"
-#     resource "*",
-#     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#     credentials: true
-#   end
-
-#   allow do
-#     origins "https://highsoaringministries.herokuapp.com"
-#     resource "*",
-#     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#     credentials: true
-#   end
-
-#   allow do
-#     origins "http://www.highsoaringministries.herokuapp.com"
-#     resource "*",
-#     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#     credentials: true
-#   end
-
-#   allow do
-#     origins "secret-melon-nagt1bephe8j9m5gs1g4h27u.herokudns.com"
-#     resource "*",
-#     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#     credentials: true
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "https://www.highsoaringministries.herokuapp.com",
+      "http://www.highsoaringministries.herokuapp.com",
+      "https://highsoaringministries.herokuapp.com",
+      "http://www.highsoaringministries.herokuapp.com",
+      "secret-melon-nagt1bephe8j9m5gs1g4h27u.herokudns.com"
+    resource "*",
+    headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
+end
