cbdd97e3665abec66fdfc1f49d58bcd3103ee9d6
Tanner Sigel <tanner@barnpros.com>
2021-11-02 15:57:07 -0700

    explicit routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,14 @@
 require 'rack/cors'
 
 Rails.application.config.hosts << ".highsoaringministries.herokuapp.com"
-Rails.application.config.hosts << /*\.highsoaringministries-api\.herokuapp\.com/
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com"
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com/sessions"
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com/posts"
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com/getposts"
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com/registrations"
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com/logout"
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com/logged_in"
+Rails.application.config.hosts << ".highsoaringministries-api.herokuapp.com/searchpost"
 Rails.application.config.hosts << ".highsoaringministries.com"
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
