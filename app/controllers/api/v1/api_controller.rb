2c94167bb35b17025c944b8dacad79e28b49e490
Tanner Sigel <tanner2379@live.com>
2021-12-29 14:59:03 -0800

    logged entire session for production debug

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::ApiController < ApplicationController
 
   def require_user
     if !user_signed_in?
-      puts(session[:user_id])
+      puts("session: " + session)
       
       render json: {
         status: 401
