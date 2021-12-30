04c77fb227af64399a2aac5afc131be9bc63aa03
Tanner Sigel <tanner2379@live.com>
2021-12-30 14:21:45 -0800

    Added secure headers configuration to session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,11 @@
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+   samesite: {
+    none: true
+   }
+  }
+end
+
 if Rails.env == "production"
   Rails.application.config.session_store :cookie_store, key: "_session_id", domain: "https://www.highsoaringministries.com", same_site: :none
 else
