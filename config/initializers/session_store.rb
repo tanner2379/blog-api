c22f9d63c877544c07fca6a4c7872df03c30269a
Tanner Sigel <tanner2379@live.com>
2021-12-30 15:08:44 -0800

    moved same site protection to cookies serializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_session_id", domain: "https://www.highsoaringministries.com", same_site: :none
-  Rails.application.config.action_dispatch.cookies_same_site_protection :none
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://www.highsoaringministries.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
