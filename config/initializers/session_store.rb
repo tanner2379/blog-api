567bb376fb43e9ce7f20355ddcc03fa25a8a8a62
Tanner Sigel <tanner2379@live.com>
2021-12-30 16:00:15 -0800

    reordered session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", secure: true
-else
+if Rails.env == "development"
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", secure: true
 end
